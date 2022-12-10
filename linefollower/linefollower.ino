8a616e5b76939662b277f8769018a31b10f4530e
ConfessorJiji <tom.github@runbox.com>
2022-12-10 20:26:20 +0100

    add some imgproc functions to linefollower ino

--- linefollower/linefollower.ino
+++ linefollower/linefollower.ino
@@ -1,6 +1,7 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
 #include <ArduinoJson.h>
+#include <esp_camera.h>
 
 //Depenencies for image processing
 #include <iostream>
@@ -8,14 +9,33 @@
 #include <cstring>
 #include <vector>
 
+
+// Pin definitions for the ESP32-CAM module
+const int CAM_PIN_PWDN = -1;
+const int CAM_PIN_RESET = -1;
+const int CAM_PIN_XCLK = 0;
+const int CAM_PIN_SIOD = 2;
+const int CAM_PIN_SIOC = 15;
+const int CAM_PIN_DATA = 4;
+
+// Pin definition for the OV2640 camera module
+const int OV2640_CAM_ADDRESS = 0x3C;
+
 // Constants for the dimensions of the image
-const int IMAGE_WIDTH = 256;
-const int IMAGE_HEIGHT = 256;
+const int IMAGE_WIDTH = 640;
+const int IMAGE_HEIGHT = 480;
 
 // The image data, stored as a 2D array of pixels
 int image[IMAGE_WIDTH][IMAGE_HEIGHT];
 
+// Buffer to store the image data
+uint8_t imageData[IMAGE_WIDTH * IMAGE_HEIGHT * 3];
+
+// Setup values for the program
+// Region of the picture to be processed
 int selectedRegion = 0;
+// Threshold for the pixel density in a given area to be considered a line
+int pxel_threshold = 100;
 
 // Replace with your own Wi-Fi credentials
 const char* ssid = "your_network_name";
@@ -82,7 +102,35 @@ std::vector<int> splitImage()
     return splitPoints;
 }
 
- 
+// Function that splits the image into 6 vertical regions and returns a vector
+// containing the x-coordinates of the split points.
+std::vector<int> splitImageVertically()
+{
+    // The size of each region is 1/6 of the image width
+    int regionSize = IMAGE_WIDTH / 6;
+
+    // The vector that will be returned, containing the x-coordinates
+    // of the split points.
+    std::vector<int> splitPoints;
+
+    // The first split point is at the left of the image
+    splitPoints.push_back(0);
+
+    // The next five split points are at the middle of the first, second,
+    // third, fourth, and fifth regions, respectively.
+    splitPoints.push_back(regionSize);
+    splitPoints.push_back(2 * regionSize);
+    splitPoints.push_back(3 * regionSize);
+    splitPoints.push_back(4 * regionSize);
+    splitPoints.push_back(5 * regionSize);
+
+    // The last split point is at the right of the image
+    splitPoints.push_back(IMAGE_WIDTH);
+
+    // Return the vector of split points.
+    return splitPoints;
+}
+
 // Calculates the point with the highest density of white pixels in the
 // region of the image bounded by the specified y-coordinates.
 std::vector<int> findHighestDensityPoint(int y1, int y2)
@@ -131,17 +179,99 @@ std::vector<int> findHighestDensityPoint(int y1, int y2)
     }
 }
 
-
-//funtion determaning the vectors between the highest density points
-std::vector<int> findVector(std::vector<int> highestDensityPoint1, std::vector<int> highestDensityPoint2)
+// Function that counts the amount of white pixels in the region of the image
+// bounded by the specified x and y-coordinates. Returns if the threshold for white pixels is met.
+bool countWhitePixels(int x1, int x2, int y1, int y2)
 {
-    //The vector between the two highest density points
-    std::vector<int> vector = {highestDensityPoint2[0] - highestDensityPoint1[0], highestDensityPoint2[1] - highestDensityPoint1[1]};
-    return vector;
+    // The current amount of white pixels
+    int whitePixels = 0;
+
+    // Loop through the region of the image bounded by the specified x and y-coordinates
+    for (int y = y1; y < y2; y++)
+    {
+        for (int x = x1; x < x2; x++)
+        {
+            // Count the white pixels at the current point
+            if (image[x][y] == 1)
+            {
+                whitePixels++;
+            }
+        }
+    }
+
+    // Return if the threshold for white pixels is met
+    if (whitePixels > pxel_threshold)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
+// Function to modify the image for processing turns it black and white and inverts the colors
+void modifyImage(esp_camera_fb_t* fb, int saturation, int contrast) {
+  // Set the color filter to grayscale
+  esp_err_t err = set_color_bar(0);
+  if (err != ESP_OK) {
+    Serial.println("Error setting color filter");
+    return;
+  }
 
+  // Set the new saturation and contrast values
+  err = set_saturation(saturation);
+  if (err != ESP_OK) {
+    Serial.println("Error setting saturation");
+    return;
+  }
+  err = set_contrast(contrast);
+  if (err != ESP_OK) {
+    Serial.println("Error setting contrast");
+    return;
+  }
 
+  // Set the image effect to invert
+  err = set_image_effect(IMAGE_EFFECT_NEGATIVE);
+  if (err != ESP_OK) {
+    Serial.println("Error setting image effect");
+    return;
+  }
+
+  // Capture a new image with the updated color filter and properties
+  camera_fb_t* newFb = esp_camera_fb_get();
+  if (!newFb) {
+    Serial.println("Error capturing image");
+    return;
+  }
+
+  // Copy the new image data to the original frame buffer
+  memcpy(fb->buf, newFb->buf, newFb->len);
+  fb->len = newFb->len;
+
+  // Release the new frame buffer
+  esp_camera_fb_return(newFb);
+}
+
+// function for thresholding the image
+esp_camera_fb_t* thresholdImage(esp_camera_fb_t* fb, uint8_t threshold) {
+  // Allocate a new frame buffer for the thresholded image
+  camera_fb_t* thresholdedFb = (camera_fb_t*) calloc(1, sizeof(camera_fb_t));
+  if (!thresholdedFb) {
+    Serial.println("Error allocating frame buffer");
+    return NULL;
+  }
+
+  // Threshold the image
+  esp_err_t err = threshold_image(fb, threshold, thresholdedFb);
+  if (err != ESP_OK) {
+    Serial.println("Error thresholding image");
+    return NULL;
+  }
+
+  // Return the thresholded image
+  return thresholdedFb;
+}
 
 
 void setup() {
@@ -156,7 +286,24 @@ void setup() {
   }
   Serial.println();
   Serial.println("Connected to Wi-Fi");
+
+  // Initialize the ESP32-CAM module
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = CAM_PIN_SIOD;
+  config.pin_d1 = CAM_PIN_SIOC;
+  config.pin_d2 = CAM_PIN_DATA;
+  config.pin_d3 = CAM_PIN_DATA;
+  config.pin_d4 = CAM_PIN_DATA;
+  config.pin_d5 = CAM_PIN_DATA;
+  config.pin_d6 = CAM_PIN_DATA;
+  config.pin_d7 = CAM_PIN_DATA;
+  config.pin_xclk = CAM_PIN_XCLK;
+  // TODO add these funtions to node red flow
 }
+
+
 
 void loop() {
   // Check if there are incoming commands from the API
@@ -172,6 +319,16 @@ void loop() {
       // TODO: Add code here to handle the incoming command
     }
   }
+
+
+  // Take a picture
+    camera_fb_t *fb = NULL;
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+
 }
 
 
