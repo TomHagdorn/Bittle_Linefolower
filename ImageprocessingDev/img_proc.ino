2eaa9007da42c19ef08e3d3560851589b29dc308
ConfessorJiji <tom.github@runbox.com>
2022-12-11 17:18:24 +0100

    Updated image proc and node red flow

--- ImageprocessingDev/img_proc.ino
+++ ImageprocessingDev/img_proc.ino
@@ -1,317 +1,287 @@
-#include <HTTPClient.h>
-#include <WiFi.h>
-#include <ArduinoJson.h>
-#include <esp_camera.h>
+/*!
+    \file camera_dump.ino
 
+    \brief Dump array captured from camera to serial.
 
-#include "soc/soc.h" //disable brownout problems
-#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+    The image captured is stored in a form of 1D array. This image is a grayscale image.
+    The contrast and exposure is set automatically.
 
-//Depenencies for image processing
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
+    \author Sameer Tuteja
+*/
 
+/*!
+    \brief Check for ESP32 board.
+*/
 
-// Pin definitions for the ESP32-CAM module
-const int CAM_PIN_PWDN = -1;
-const int CAM_PIN_RESET = -1;
-const int CAM_PIN_XCLK = 0;
-const int CAM_PIN_SIOD = 2;
-const int CAM_PIN_SIOC = 15;
-const int CAM_PIN_DATA = 4;
+#if !defined ESP32
+#error Wrong board selected
+#endif
 
-// Pin definition for the OV2640 camera module
-const int OV2640_CAM_ADDRESS = 0x3C;
+#define CAMERA_MODEL_AI_THINKER
 
-// Constants for the dimensions of the image
-const int IMAGE_WIDTH = 640;
-const int IMAGE_HEIGHT = 480;
+#include "esp_camera.h" ///< Header file for camera obtained from https://github.com/espressif/
 
-// The image data, stored as a 2D array of pixels
-int image[IMAGE_WIDTH][IMAGE_HEIGHT];
+#include "driver/ledc.h" ///< To enable onboard Illumination/flash LED pin attached on 4
 
-// Buffer to store the image data
-uint8_t imageData[IMAGE_WIDTH * IMAGE_HEIGHT * 3];
+#include "soc/soc.h"          //! Used to disable brownout detection
+#include "soc/rtc_cntl_reg.h" //! Used to disable brownout detection
 
-// Setup values for the program
-// Region of the picture to be processed
-int selectedRegion = 0;
-// Threshold for the pixel density in a given area to be considered a line
-int pxel_threshold = 100;
+//! Image resolution:
+/*!
+    default = "const framesize_t FRAME_SIZE_IMAGE = FRAMESIZE_VGA"
 
-// Replace with your own Wi-Fi credentials
-const char* ssid = "your_network_name";
-const char* password = "your_network_password";
+    Other available Frame Sizes:
+    160x120 (QQVGA), 128x160 (QQVGA2), 176x144 (QCIF), 240x176 (HQVGA),
+    320x240 (QVGA), 400x296 (CIF), 640x480 (VGA, default), 800x600 (SVGA),
+    1024x768 (XGA), 1280x1024 (SXGA), 1600x1200 (UXGA)
+*/
 
-// Replace with your own API URL and port
-const char* host = "your_api_url";
-const int port = 80; // or whichever port your API is running on
+const framesize_t FRAME_SIZE_IMAGE = FRAMESIZE_QQVGA;
 
-// Functions
+//! Image Format
+/*!
+    Other Available formats:
+    YUV422, GRAYSCALE, RGB565, JPEG, RGB888
+*/
 
+#define PIXFORMAT PIXFORMAT_GRAYSCALE;
 
-// Function that splits the image into 3 horizontal regions and returns a vector
-// containing the y-coordinates of the split points.
-std::vector<int> splitImage()
+#define WIDTH 160  ///< Image size Width
+#define HEIGHT 120 ///< Image size Height
+
+//! Camera exposure
+/*!
+    Range: (0 - 1200)
+    If gain and exposure both set to zero then auto adjust is enabled
+*/
+
+int cameraImageExposure = 0;
+
+//! Image gain
+/*!
+    Range: (0 - 30)
+    If gain and exposure both set to zero then auto adjust is enabled
+*/
+int cameraImageGain = 0;
+
+const uint8_t ledPin = 4;                  ///< onboard Illumination/flash LED pin (4)
+int ledBrightness = 16;                    ///< Initial brightness (0 - 255)
+const int pwmFrequency = 50000;            ///< PWM settings for ESP32
+const uint8_t ledChannel = LEDC_CHANNEL_0; ///< Camera timer0
+const uint8_t pwmResolution = 8;           ///< resolution (8 = from 0 to 255)
+
+const int serialSpeed = 115200; ///< Serial data speed to use
+
+//! Camera setting
+/*!
+    Camera settings for CAMERA_MODEL_AI_THINKER OV2640
+    Based on CameraWebServer sample code by ESP32 Arduino
+
+*/
+#if defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+#endif
+
+uint32_t lastCamera = 0; ///< To store time value for repeated capture
+/**************************************************************************/
+/*!
+  \brief  Setup function
+
+  Initialization for following:
+    disable Brownout detection
+    camera
+
+*/
+/**************************************************************************/
+void setup()
 {
-    // The size of each region is 1/3 of the image height
-    int regionSize = IMAGE_HEIGHT / 3;
+    Serial.begin(serialSpeed); ///< Initialize serial communication
+    Serial.println("\n");
 
-    // The vector that will be returned, containing the y-coordinates
-    // of the split points.
-    std::vector<int> splitPoints;
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); ///< Disable 'brownout detector'
 
-    // The first split point is at the top of the image
-    splitPoints.push_back(0);
-
-    // The next two split points are at the middle and bottom of the
-    // first and second regions, respectively.
-    splitPoints.push_back(regionSize);
-    splitPoints.push_back(2 * regionSize);
-
-    // The last split point is at the bottom of the image
-    splitPoints.push_back(IMAGE_HEIGHT);
-
-    // Return the vector of split points.
-    return splitPoints;
-}
-
-// Function that splits the image into 6 vertical regions and returns a vector
-// containing the x-coordinates of the split points.
-std::vector<int> splitImageVertically()
-{
-    // The size of each region is 1/6 of the image width
-    int regionSize = IMAGE_WIDTH / 6;
-
-    // The vector that will be returned, containing the x-coordinates
-    // of the split points.
-    std::vector<int> splitPoints;
-
-    // The first split point is at the left of the image
-    splitPoints.push_back(0);
-
-    // The next five split points are at the middle of the first, second,
-    // third, fourth, and fifth regions, respectively.
-    splitPoints.push_back(regionSize);
-    splitPoints.push_back(2 * regionSize);
-    splitPoints.push_back(3 * regionSize);
-    splitPoints.push_back(4 * regionSize);
-    splitPoints.push_back(5 * regionSize);
-
-    // The last split point is at the right of the image
-    splitPoints.push_back(IMAGE_WIDTH);
-
-    // Return the vector of split points.
-    return splitPoints;
-}
-
-// Calculates the point with the highest density of white pixels in the
-// region of the image bounded by the specified y-coordinates.
-std::vector<int> findHighestDensityPoint(int y1, int y2)
-{
-    // The current highest density of white pixels
-    int highestDensity = 0;
-
-    // The coordinates of the point with the highest density of white pixels
-    int highestDensityX = 0;
-    int highestDensityY = 0;
-
-    // Loop through the region of the image bounded by the specified y-coordinates
-    for (int y = y1; y < y2; y++)
+    Serial.print(("\nInitialising camera: "));
+    if (initialiseCamera())
     {
-        for (int x = 0; x < IMAGE_WIDTH; x++)
-        {
-            // Calculate the density of white pixels at the current point
-            int density = 0;
-            for (int dy = -1; dy <= 1; dy++)
-            {
-                for (int dx = -1; dx <= 1; dx++)
-                {
-                    int xx = x + dx;
-                    int yy = y + dy;
-                    if (xx >= 0 && xx < IMAGE_WIDTH && yy >= 0 && yy < IMAGE_HEIGHT)
-                    {
-                        if (image[xx][yy] == 1)
-                        {
-                            density++;
-                        }
-                    }
-                }
-            }
-
-            // Update the current highest density and coordinates if necessary
-            if (density > highestDensity)
-            {
-                highestDensity = density;
-                highestDensityX = x;
-                highestDensityY = y;
-                //Write vector of highest density point
-                std::vector<int> highestDensityPoint = {highestDensityX, highestDensityY};
-                return highestDensityPoint;
-            }
-        }
-    }
-}
-
-// Function that counts the amount of white pixels in the region of the image
-// bounded by the specified x and y-coordinates. Returns if the threshold for white pixels is met.
-bool countWhitePixels(int x1, int x2, int y1, int y2)
-{
-    // The current amount of white pixels
-    int whitePixels = 0;
-
-    // Loop through the region of the image bounded by the specified x and y-coordinates
-    for (int y = y1; y < y2; y++)
-    {
-        for (int x = x1; x < x2; x++)
-        {
-            // Count the white pixels at the current point
-            if (image[x][y] == 1)
-            {
-                whitePixels++;
-            }
-        }
-    }
-
-    // Return if the threshold for white pixels is met
-    if (whitePixels > pxel_threshold)
-    {
-        return true;
+        Serial.println("OK");
     }
     else
     {
+        Serial.println("Error!");
+    }
+
+    setupOnBoardFlash();
+    Serial.println("Setup complete\n");
+    setLedBrightness(ledBrightness);
+}
+/**************************************************************************/
+/*!
+  \brief  Loop function
+  Capture image every 10 seconds
+*/
+/**************************************************************************/
+void loop()
+{
+
+    if ((unsigned long)(millis() - lastCamera) >= 10000UL)
+    {
+        lastCamera = millis(); // reset timer
+        capture_still();       // function call -> capture image
+    }
+}
+/**************************************************************************/
+/**
+  Initialise Camera
+  Set camera parameters
+  Based on CameraWebServer sample code by ESP32 Arduino
+  \return true: successful, false: failed
+ */
+/**************************************************************************/
+bool initialiseCamera()
+{
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT;
+    config.frame_size = FRAME_SIZE_IMAGE;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+
+    // Check the esp32cam board has a PSRAM chip installed (extra memory used for storing captured images)
+    // Note: if not using "AI thinker esp32 cam" in the Arduino IDE, PSRAM must be enabled
+    if (!psramFound())
+    {
+        Serial.println("Warning: No PSRam found so defaulting to image size 'CIF'");
+        config.frame_size = FRAMESIZE_CIF;
+    }
+
+    esp_err_t camerr = esp_camera_init(&config); // initialise the camera
+    if (camerr != ESP_OK)
+    {
+        Serial.printf("ERROR: Camera init failed with error 0x%x", camerr);
+    }
+
+    cameraImageSettings(); // Apply custom camera settings
+
+    return (camerr == ESP_OK); // Return boolean result of camera initialisation
+}
+/**************************************************************************/
+/**
+  Camera Image Settings
+  Set Image parameters
+  Based on CameraWebServer sample code by ESP32 Arduino
+  \return true: successful, false: failed
+ */
+/**************************************************************************/
+bool cameraImageSettings()
+{
+
+    sensor_t *s = esp_camera_sensor_get();
+    if (s == NULL)
+    {
+        Serial.println("Error: problem reading camera sensor settings");
+        return 0;
+    }
+
+    // if both set to zero enable auto adjust
+    if (cameraImageExposure == 0 && cameraImageGain == 0)
+    {
+        // enable auto adjust
+        s->set_gain_ctrl(s, 1);     // auto gain on
+        s->set_exposure_ctrl(s, 1); // auto exposure on
+        s->set_awb_gain(s, 1);      // Auto White Balance enable (0 or 1)
+    }
+    else
+    {
+        // Apply manual settings
+        s->set_gain_ctrl(s, 0);                   // auto gain off
+        s->set_awb_gain(s, 1);                    // Auto White Balance enable (0 or 1)
+        s->set_exposure_ctrl(s, 0);               // auto exposure off
+        s->set_agc_gain(s, cameraImageGain);      // set gain manually (0 - 30)
+        s->set_aec_value(s, cameraImageExposure); // set exposure manually  (0-1200)
+    }
+
+    return true;
+}
+/**************************************************************************/
+/**
+  Camera Image Settings
+  Set Image parameters
+  Based on CameraWebServer sample code by ESP32 Arduino
+  \return true: successful, false: failed
+ */
+/**************************************************************************/
+bool capture_still()
+{
+    camera_fb_t *frame = esp_camera_fb_get();
+
+    if (!frame)
         return false;
+
+    for (int row_index = 0; row_index < HEIGHT; ++row_index)
+    {
+        for (int col_index = 0; col_index < WIDTH; ++col_index)
+        {
+            Serial.print(frame->buf[WIDTH * row_index + col_index]);
+            Serial.print(" ");
+        }
+        Serial.print("\n");
     }
+    Serial.println();
+	esp_camera_fb_return(frame);
+    return true;
 }
+/**************************************************************************/
+/**
+  Setup On Board Flash
+  Initialize on board LED with pwm channel
+ */
+/**************************************************************************/
 
-// Function to modify the image for processing turns it black and white and inverts the colors
-void modifyImage(esp_camera_fb_t* fb, int saturation, int contrast) {
-  // Set the color filter to grayscale
-  esp_err_t err = set_color_bar(0);
-  if (err != ESP_OK) {
-    Serial.println("Error setting color filter");
-    return;
-  }
-
-  // Set the new saturation and contrast values
-  err = set_saturation(saturation);
-  if (err != ESP_OK) {
-    Serial.println("Error setting saturation");
-    return;
-  }
-  err = set_contrast(contrast);
-  if (err != ESP_OK) {
-    Serial.println("Error setting contrast");
-    return;
-  }
-
-  // Set the image effect to invert
-  err = set_image_effect(IMAGE_EFFECT_NEGATIVE);
-  if (err != ESP_OK) {
-    Serial.println("Error setting image effect");
-    return;
-  }
-
-  // Capture a new image with the updated color filter and properties
-  camera_fb_t* newFb = esp_camera_fb_get();
-  if (!newFb) {
-    Serial.println("Error capturing image");
-    return;
-  }
-
-  // Copy the new image data to the original frame buffer
-  memcpy(fb->buf, newFb->buf, newFb->len);
-  fb->len = newFb->len;
-
-  // Release the new frame buffer
-  esp_camera_fb_return(newFb);
+void setupOnBoardFlash()
+{
+    ledcSetup(ledChannel, pwmFrequency, pwmResolution);
+    ledcAttachPin(ledPin, ledChannel);
 }
-
-// function for thresholding the image
-esp_camera_fb_t* thresholdImage(esp_camera_fb_t* fb, uint8_t threshold) {
-  // Allocate a new frame buffer for the thresholded image
-  camera_fb_t* thresholdedFb = (camera_fb_t*) calloc(1, sizeof(camera_fb_t));
-  if (!thresholdedFb) {
-    Serial.println("Error allocating frame buffer");
-    return NULL;
-  }
-
-  // Threshold the image
-  esp_err_t err = threshold_image(fb, threshold, thresholdedFb);
-  if (err != ESP_OK) {
-    Serial.println("Error thresholding image");
-    return NULL;
-  }
-
-  // Return the thresholded image
-  return thresholdedFb;
+/**************************************************************************/
+/**
+  Set Led Brightness
+  Set pwm value to change brightness of LED
+ */
+/**************************************************************************/
+void setLedBrightness(byte ledBrightness)
+{
+    ledcWrite(ledChannel, ledBrightness);
 }
-
-
-
-
-
-void setup() {
-  // Initialize serial communication
-  Serial.begin(115200);
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.println("Connected to Wi-Fi");
-
-  // Initialize the ESP32-CAM module
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = CAM_PIN_SIOD;
-  config.pin_d1 = CAM_PIN_SIOC;
-  config.pin_d2 = CAM_PIN_DATA;
-  config.pin_d3 = CAM_PIN_DATA;
-  config.pin_d4 = CAM_PIN_DATA;
-  config.pin_d5 = CAM_PIN_DATA;
-  config.pin_d6 = CAM_PIN_DATA;
-  config.pin_d7 = CAM_PIN_DATA;
-  config.pin_xclk = CAM_PIN_XCLK;
-  // TODO add these funtions to node red flow
-  // TODO Add a statemachine calling all the functions
-  
-}
-
-
-
-void loop() {
-  // Check if there are incoming commands from the API
-  if (WiFi.available()) {
-    // Read the incoming command
-    String command = WiFi.readStringUntil('\r');
-    // Parse the JSON command
-    DynamicJsonDocument jsonDoc(1024);
-    DeserializationError error = deserializeJson(jsonDoc, command);
-    if (error) {
-      Serial.println("Error parsing JSON command: " + error.c_str());
-    } else {
-      // TODO: Add code here to handle the incoming command
-    }
-  }
-
-
-  // Take a picture
-    camera_fb_t *fb = NULL;
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      return;
-    }
-
-    
-
-}
-
-
