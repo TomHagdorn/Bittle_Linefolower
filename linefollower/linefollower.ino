fa0dc7407d28a5ac609d014fa09e175c02bc924c
ConfessorJiji <tom.github@runbox.com>
2023-01-10 17:48:07 +0100

    Revert "	everything works exept avoid obstacle"
    This reverts commit 0f3b33a324892c8eba9529398d2b7627d3a69c06.

--- linefollower/linefollower.ino
+++ linefollower/linefollower.ino
@@ -481,14 +481,380 @@ int auto_threshold(camera_fb_t *fb, int min_white_pixels, int max_white_pixels)
         }
     }
 
-  // Once the desired range has been reached, apply the threshold to the image
-  for (int i = 0; i < width * height; i++) {
-    if (data[i] > threshold) {
-      data[i] = 255;
-    } else {
-      data[i] = 0;
+    // Once the desired range has been reached, apply the threshold to the image
+    for (int i = 0; i < width * height; i++)
+    {
+        if (data[i] > threshold)
+        {
+            data[i] = 255;
+        }
+        else
+        {
+            data[i] = 0;
+        }
     }
-  }
+    return threshold;
 }
 
+/**************************************************************************/
+/**
+  Line Follower
+  Follow a line using the camera
+  \param fb: pointer to the frame buffer
+  \return true if a line is found, false otherwise
+ */
+/**************************************************************************/
+bool line_follower(const camera_fb_t *fb)
+{
+    // calculate the starting and ending fractions for the 3/4 to 1 portion of the frame
+    double start_fraction = 3.0 / 4.0;
+    double end_fraction = 1.0;
 
+    // get the middle point for the 3/4 to 1 portion of the frame
+    int middle_point = get_middle_point(fb, start_fraction, end_fraction);
+    if (middle_point == -1)
+    {
+        // no line was found, so stop the robot
+        Serial.print("kp");
+        return false;
+    }
+
+    // if the point of highest density is in one of the 3/7th of the left side of the picture
+    if (middle_point < fb->width * 4 / 11)
+    {
+        // move the robot to the left
+        Serial.print("kwkL");
+        return true;
+    }
+    // if the point of highest density is in one of the 3/7th of the right side of the picture
+    else if (middle_point >= fb->width * 8 / 11)
+    {
+        // move the robot to the right
+        Serial.print("kwkR");
+        return true;
+    }
+    // if the point of highest density is within the 4/7th in the middle
+    else
+    {
+        // move the robot forward
+        Serial.print("kwkF");
+        return true;
+    }
+}
+
+/**************************************************************************/
+/**
+  Get Middle Point
+  Get the middle point of the white pixels in the bottom third of the image
+  \param fb: pointer to the frame buffer
+  \return the x-coordinate of the middle point
+ */
+/**************************************************************************/
+int get_middle_point(const camera_fb_t *fb, double start_fraction, double end_fraction)
+{
+    // initialize the starting and ending x-coordinates to zero
+    int start_x = 0;
+    int end_x = 0;
+    // flag to track if we have found the start of the white pixels
+    bool found_start = false;
+    // variable to track the number of consecutive non-white pixels
+    int consecutive_non_white = 0;
+    // variable to track the number of consecutive white pixels to check row validity
+    int consecutive_white = 0;
+    // row counter
+    int valid_row_counter = 0;
+    // vector to store the middle points of each row
+    std::vector<int> middle_points;
+
+    // calculate the start and end rows based on the start and end fractions
+    int start_row = fb->height * start_fraction;
+    int end_row = fb->height * end_fraction;
+
+    // iterate over the rows in the specified range
+    for (int y = start_row; y < end_row; y++)
+    {
+        // iterate over the columns in the current row
+        for (int x = 0; x < fb->width; x++)
+        {
+            // get the current pixel value
+            uint8_t pixel = fb->buf[y * fb->width + x];
+
+            // if the pixel is white (i.e., its value is above the threshold)
+            if (pixel == 255)
+            {
+                // if we haven't found the start of the white pixels yet,
+                // set the start x-coordinate to the current x-coordinate
+                consecutive_white++;
+                // reset the consecutive non-white pixels counter
+                consecutive_non_white = 0;
+                // if we haven't found the start of the white pixels yet,
+                if (!found_start)
+                {
+                    start_x = x;
+                    found_start = true;
+                }
+                // update the ending x-coordinate to the current x-coordinate
+                end_x = x;
+            }
+            else
+            {
+                // increment the consecutive non-white pixels counter
+                consecutive_non_white++;
+            }
+            // if we have seen 15 consecutive non-white pixels, set the end x-coordinate to
+            // the current x-coordinate minus 15
+            if (consecutive_non_white >= 8)
+            {
+                end_x = x - 15;
+                consecutive_non_white = 0;
+                found_start = false;
+            }
+            // if we have seen 40 consecutive white pixels, then we have found a valid row
+            if (consecutive_white >= 20 && found_start)
+            {
+                valid_row_counter++;
+                // calculate the middle x-coordinate of the white pixels
+                int middle_point = (end_x + start_x) / 2;
+                // add the middle point to the vector
+                middle_points.push_back(middle_point);
+            }
+        }
+    }
+    // if we have found at least one valid row
+    if (valid_row_counter > 5)
+    {
+        // sort the middle points
+        std::sort(middle_points.begin(), middle_points.end());
+        // calculate the median middle point
+        int median_middle_point = middle_points[middle_points.size() / 2];
+        // return the median middle point
+        return median_middle_point;
+    }
+    // if we haven't found any valid rows, return -1
+    else
+    {
+        return -1;
+    }
+}
+
+/**************************************************************************/
+/**
+  Detect Obstacle
+  Detect an obstacle using the camera
+  \param fb: pointer to the frame buffer
+  \return true if an obstacle is found, false otherwise
+ */
+bool detect_obstacle(const camera_fb_t *fb)
+{
+    // calculate the starting and ending fractions for the top, middle, and bottom thirds of the frame
+    double start_fraction_top = 0.0;
+    double end_fraction_top = 1.0 / 3.0;
+    double start_fraction_middle = 1.0 / 3.0;
+    double end_fraction_middle = 2.0 / 3.0;
+    double start_fraction_bottom = 2.0 / 3.0;
+    double end_fraction_bottom = 1.0;
+
+    // get the middle points for the top, middle, and bottom thirds of the frame
+    int middle_point_top = get_middle_point(fb, start_fraction_top, end_fraction_top);
+    int middle_point_middle = get_middle_point(fb, start_fraction_middle, end_fraction_middle);
+    int middle_point_bottom = get_middle_point(fb, start_fraction_bottom, end_fraction_bottom);
+
+    // if the middle point for the bottom third of the frame is not -1 (i.e., a valid row was found),
+    // and the middle points for the top and middle thirds are -1 (i.e., no valid rows were found),
+    // then we consider an obstacle to have been detected
+    if (middle_point_bottom != -1 && middle_point_top == -1 && middle_point_middle == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// climbing over the obstacle
+// void avoid_obstacle() {
+//     // keep following the line until it is lost for one second
+//     unsigned long start_time = millis();
+//     while (line_follower()) {
+//         if (millis() - start_time > 1000) {
+//             break;
+//         }
+//     }
+
+//     // line has been lost for one second, so execute custom command
+//     //execute_custom_command();   David still has to write this function
+// }
+
+void avoid_obstacle()
+{
+    // stop the robot
+    Serial.print("kp");
+
+    // turn the robot left for a certain amount of time
+    unsigned long start_time = millis();
+    Serial.print("kwkL");
+    while (millis() - start_time < 500)
+    {
+    }
+    start_time = millis();
+    // turn the robot right until the line is found
+    Serial.print("kwkR");
+    while (get_middle_point(fb, 1.0 / 3.0, 2.0 / 3.0) < 0 || millis() - start_time < 500)
+    {
+    }
+    // turn the robot left for a certain amount of time
+    start_time = millis();
+    Serial.print("kwkL");
+    while (millis() - start_time < 200)
+    {
+    }
+    Serial.print("kp");
+}
+
+void recover()
+{
+    unsigned long start_time = millis();
+    // keep turning the robot left and walking backwards until the line is found
+    Serial.print("kwkB");
+    while (!line_follower(fb) || millis() - start_time < 200)
+    {
+    }
+    Serial.print("kp");
+}
+
+void cool_move()
+{
+    // TODO David writes cool move and then we test
+    //write function to turn on the spot
+}
+
+void crossFinishLine()
+{
+    // walk backwards for a certain amount of time
+    Serial.println("kwkF");
+    unsigned long start_time = millis();
+    while (millis() - start_time < 400)
+    {
+    }
+    Serial.println("kp");
+}
+/**************************************************************************/
+/**
+ * Update the current state of the line follower.
+ *
+ * This function updates the current state of the line follower based on the
+ * current conditions of the line, the presence of obstacles, and the presence
+ * of the finish line. It then executes the appropriate behavior based on the
+ * current state.
+ *
+ * The possible states are:
+ * - FOLLOW_LINE: Follow the line using the camera.
+ * - AVOID_OBSTACLE: Avoid an obstacle by walking around it.
+ * - CROSS_FINISH_LINE: Cross the finish line.
+ * - FINISH: The robot has finished the course.
+ * - RECOVER_FROM_NO_LINE: Recover from losing the line by walking backwards in a curve.
+ *
+ * \param fb: pointer to the frame buffer.
+ */
+/**************************************************************************/
+void update()
+{
+    static unsigned long startTime = millis();
+
+    switch (currentState)
+    {
+    case FOLLOW_LINE:
+        Serial.println("\nFOLLOW_LINE");
+        if (line_follower(fb))
+        {
+            lastStateChangeTime = millis();
+            // line follower returned true, indicating that the line was found
+            if (detect_obstacle(fb))
+            {
+                currentState = AVOID_OBSTACLE;
+                lastStateChangeTime = millis();
+            }
+            else if (check_for_horizontal_line(fb) && (millis() - startTime >= 60000))
+            {
+                currentState = CROSS_FINISH_LINE;
+                lastStateChangeTime = millis();
+            }
+        }
+        else
+        {
+            if (millis() - lastStateChangeTime >= 3000)
+            {
+                // line follower returned false, indicating that the line was not found
+                // and at least 3 seconds have passed since the last state change
+                currentState = RECOVER_FROM_NO_LINE;
+                lastStateChangeTime = millis();
+            }
+        }
+        break;
+    case AVOID_OBSTACLE:
+        Serial.println("\nAVOID_OBSTACLE");
+        avoid_obstacle();
+        currentState = FOLLOW_LINE;
+        lastStateChangeTime = millis();
+        
+        break;
+    case CROSS_FINISH_LINE:
+        Serial.println("\nCROSS_FINISH_LINE");
+        crossFinishLine();
+        if (finish_line_crossed == false)
+        {
+            finish_line_crossed = true;
+            cool_move();
+            currentState = FOLLOW_LINE;
+            lastStateChangeTime = millis();
+        }
+        else
+        {
+            currentState = FINISH;
+        }
+        break;
+    case RECOVER_FROM_NO_LINE:
+    //TODO recovery spams the serial monitor, with kp commands find the issue and fix it
+    // needs to loop somwhere wreid bc the update finction is called every 600ms and the kp command is sent at a rate way higher than that
+        Serial.println("\nRECOVER_FROM_NO_LINE");
+        recover();
+        // line follower returned true, indicating that the line was found
+        currentState = FOLLOW_LINE;
+        lastStateChangeTime = millis();
+        break;
+    case FINISH:
+        // Robot has finished, do nothing
+        break;
+    }
+}
+
+void publishPictureToServer(const camera_fb_t *fb, WiFiServer server)
+{
+    // Wait for a client to connect
+    WiFiClient client = server.available();
+    while (!client)
+    {
+        delay(1);
+    }
+
+    // Send the response header
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: image/jpeg");
+    client.println("Content-Length: " + String(fb->len));
+    client.println();
+
+    // Send the image data
+    client.write((uint8_t *)fb->buf, fb->len);
+}
+
+void setupWebServer(WiFiServer server)
+{
+    // Start the server
+    server.begin();
+
+    // Print the URL to the serial monitor
+    Serial.print("Visit http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("/ to view the image");
+}
