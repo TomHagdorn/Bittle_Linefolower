876932b67a4a3a65710bee9c4634cd2053e84934
ConfessorJiji <tom.github@runbox.com>
2023-02-28 20:28:40 +0100

    Cleaned up a bit more

--- linefollower/linefollower.ino
+++ linefollower/linefollower.ino
@@ -17,118 +17,25 @@
 #error Wrong board selected
 #endif
 
-#define CAMERA_MODEL_AI_THINKER
-
-
-#include "esp_camera.h" ///< Header file for camera obtained from https://github.com/espressif/
-
-#include "driver/ledc.h" ///< To enable onboard Illumination/flash LED pin attached on 4
 
 #include "soc/soc.h"          //! Used to disable brownout detection
 #include "soc/rtc_cntl_reg.h" //! Used to disable brownout detection
 
 #include <vector>
-// Depenencies for image processing
-
-#include <ArduinoJson.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <HTTPClient.h>
-
-#include <cmath>
 
 //My files 
+#include "camera_setup.h"
 #include "control.h"
 #include "node_red.h"
 
 
 
-#define SSID "esp32_server"
-#define PWD "123456789"
-
-
-
 unsigned long stateTime = 0;
 
-//WebServer * server = new WebServer(80);
-//WebServer server(80);
-
-//! Image resolution:
-/*!
-    default = "const framesize_t FRAME_SIZE_IMAGE = FRAMESIZE_VGA"
-    Other available Frame Sizes:
-    160x120 (QQVGA), 128x160 (QQVGA2), 176x144 (QCIF), 240x176 (HQVGA),
-    320x240 (QVGA), 400x296 (CIF), 640x480 (VGA, default), 800x600 (SVGA),
-    1024x768 (XGA), 1280x1024 (SXGA), 1600x1200 (UXGA)
-*/
-
-const framesize_t FRAME_SIZE_IMAGE = FRAMESIZE_QQVGA; // FRAMESIZE_QQVGA
-camera_fb_t *fb;
-
-//! Image Format
-/*!
-    Other Available formats:
-    YUV422, GRAYSCALE, RGB565, JPEG, RGB888
-*/
-
-#define PIXFORMAT PIXFORMAT_GRAYSCALE;
-
-
-
-#define IMAGE_WIDTH 160  ///< Image size Width
-#define IMAGE_HEIGHT 120 ///< Image size Height
-
-//! Camera exposure
-/*!
-    Range: (0 - 1200)
-    If gain and exposure both set to zero then auto adjust is enabled
-*/
-
-int cameraImageExposure = 0;
-
-//! Image gain
-/*!
-    Range: (0 - 30)
-    If gain and exposure both set to zero then auto adjust is enabled
-*/
-int cameraImageGain = 0;
-
-const uint8_t ledPin = 4;                  ///< onboard Illumination/flash LED pin (4)
-int ledBrightness = 8;                     ///< Initial brightness (0 - 255)
-const int pwmFrequency = 50000;            ///< PWM settings for ESP32
-const uint8_t ledChannel = LEDC_CHANNEL_0; ///< Camera timer0
-const uint8_t pwmResolution = 8;           ///< resolution (8 = from 0 to 255)
-
-const int serialSpeed = 115200; ///< Serial data speed to use
-
-// minimum line lenth for line detection
-const int min_line_length = 10;
-
-//! Camera setting
-/*!
-    Camera settings for CAMERA_MODEL_AI_THINKER OV2640
-    Based on CameraWebServer sample code by ESP32 Arduino
-*/
-#if defined(CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
-#endif
-
 uint32_t lastCamera = 0; ///< To store time value for repeated capture
+
+//define state change time
+static unsigned long lastStateChangeTime = 0;
 
 // variables updated by nodered
 //int pixel_threshold = 125;
@@ -170,12 +77,6 @@ State currentState = FOLLOW_LINE;
 bool finish_line_crossed = false;
 // define the wifi server
 
-
-
-
-
-//define state change time
-static unsigned long lastStateChangeTime = 0;
 /**************************************************************************/
 /*!
   \brief  Setup function
@@ -230,7 +131,6 @@ void setup()
     Change_Treshold_value();
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
     pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-    //Nodered setup
 
     
 
@@ -248,21 +148,21 @@ void loop()
     {   
         
 
-        esp_err_t res = camera_capture();
-        server.handleClient();
+        // esp_err_t res = camera_capture();
+        // server.handleClient();
 
-        if (res == ESP_OK)
-        {   
-            //Serial.println(pixel_threshold);
-            //update();
+        // if (res == ESP_OK)
+        // {   
+        //     //Serial.println(pixel_threshold);
+        //     //update();
             
-            // print image to serial monitor
-            //capture_still(fb);
+        //     // print image to serial monitor
+        //     //capture_still(fb);
             
 
-            // publishes the image to the server
-            // publishPictureToServer(fb, server);
-        }
+        //     // publishes the image to the server
+        //     // publishPictureToServer(fb, server);
+        // }
         // return the frame buffer back to the driver for reuse
         //esp_camera_fb_return(fb);
         
@@ -271,142 +171,6 @@ void loop()
         lastCamera = millis();
     }
 }
-/**************************************************************************/
-/**
-  Initialise Camera
-  Set camera parameters
-  Based on CameraWebServer sample code by ESP32 Arduino
-  \return true: successful, false: failed
- */
-/**************************************************************************/
-bool initialiseCamera()
-{
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT;
-    config.frame_size = FRAME_SIZE_IMAGE;
-    config.jpeg_quality = 10;
-    config.fb_count = 1;
-
-    // Check the esp32cam board has a PSRAM chip installed (extra memory used for storing captured images)
-    // Note: if not using "AI thinker esp32 cam" in the Arduino IDE, PSRAM must be enabled
-    if (!psramFound())
-    {
-        // Serial.println("Warning: No PSRam found so defaulting to image size 'CIF'");
-        config.frame_size = FRAMESIZE_CIF;
-    }
-
-    esp_err_t camerr = esp_camera_init(&config); // initialise the camera
-    if (camerr != ESP_OK)
-    {
-        Serial.printf("ERROR: Camera init failed with error 0x%x", camerr);
-    }
-
-    cameraImageSettings(); // Apply custom camera settings
-
-    return (camerr == ESP_OK); // Return boolean result of camera initialisation
-}
-/**************************************************************************/
-/**
-  Camera Image Settings
-  Set Image parameters
-  Based on CameraWebServer sample code by ESP32 Arduino
-  \return true: successful, false: failed
- */
-/**************************************************************************/
-bool cameraImageSettings()
-{
-
-    sensor_t *s = esp_camera_sensor_get();
-    if (s == NULL)
-    {
-        // Serial.println("Error: problem reading camera sensor settings");
-        return 0;
-    }
-
-    // if both set to zero enable auto adjust
-    if (cameraImageExposure == 0 && cameraImageGain == 0)
-    {
-        // enable auto adjust
-        s->set_gain_ctrl(s, 1);     // auto gain on
-        s->set_exposure_ctrl(s, 1); // auto exposure on
-        s->set_awb_gain(s, 1);      // Auto White Balance enable (0 or 1)
-    }
-    else
-    {
-        // Apply manual settings
-        s->set_gain_ctrl(s, 0);                   // auto gain off
-        s->set_awb_gain(s, 1);                    // Auto White Balance enable (0 or 1)
-        s->set_exposure_ctrl(s, 0);               // auto exposure off
-        s->set_agc_gain(s, cameraImageGain);      // set gain manually (0 - 30)
-        s->set_aec_value(s, cameraImageExposure); // set exposure manually  (0-1200)
-    }
-
-    return true;
-}
-/**************************************************************************/
-/**
-  Camera Image Settings
-  Set Image parameters
-  Based on CameraWebServer sample code by ESP32 Arduino
-  \return true: successful, false: failed
- */
-/**************************************************************************/
-esp_err_t camera_capture()
-{
-    // acquire a frame
-    fb = esp_camera_fb_get();
-    ESP_LOGE(TAG, "Camera Capture in progress");
-    if (!fb)
-    {
-        ESP_LOGE(TAG, "Camera Capture Failed");
-        return ESP_FAIL;
-    }
-
-
-    return ESP_OK;
-}
-/**************************************************************************/
-/**
-  Setup On Board Flash
-  Initialize on board LED with pwm channel
- */
-/**************************************************************************/
-
-void setupOnBoardFlash()
-{
-    ledcSetup(ledChannel, pwmFrequency, pwmResolution);
-    ledcAttachPin(ledPin, ledChannel);
-}
-/**************************************************************************/
-/**
-  Set Led Brightness
-  Set pwm value to change brightness of LED
- */
-/**************************************************************************/
-void setLedBrightness(byte ledBrightness)
-{
-    ledcWrite(ledChannel, ledBrightness);
-}
-
 
 /**************************************************************************/
 /**
@@ -495,44 +259,4 @@ void update()
         break;
     }
 }
-
-/*void setup_wifi() {
-    Serial.println("Configuring AP...");
-    if (!WiFi.softAP(SSID, PWD)) Serial.println("AP Config failed.");
-
-    // print ssid and pwd
-    Serial.printf("SSID - %s\n", SSID);
-    Serial.printf("PWD  - %s\n", PWD);
-}
-
-
-void setup_server() {
-    // server will do the following every time [esp32-ip]/image is requested:
-    server.on(F("/image"), [&]() {
-        // send message on serial for debugging
-        // take picture
-        camera_fb_t * frame_buffer = esp_camera_fb_get();
-        threshold_image(frame_buffer, pixel_threshold);
-
-        // convert frame to bmp
-        uint8_t * bmp_buffer = NULL;
-        size_t bmp_buffer_length = 0;
-        frame2bmp(frame_buffer, &bmp_buffer, &bmp_buffer_length);
-
-        // send image
-        server.send_P(200, "image/bmp", (const char *)bmp_buffer, bmp_buffer_length);
-
-        // free memory and return buffer
-        // note that the picture is actually taken when you return the frame buffer
-        free(bmp_buffer);
-        esp_camera_fb_return(frame_buffer);
-    });
-
-    // start server
-    server.begin();
-
-    // print ip
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.printf("IP   - %s\n", myIP.toString());
-}*/
 
